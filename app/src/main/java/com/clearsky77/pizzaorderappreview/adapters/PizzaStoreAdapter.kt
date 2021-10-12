5aee7ba743219ed19e6631cbe2214267230e29b2
clearsky77 <clearsky3264@gmail.com>
2021-10-12 21:42:44 +0900

    피자가게 어댑터 뼈대 작업 완료

--- app/src/main/java/com/clearsky77/pizzaorderappreview/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/adapters/PizzaStoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.clearsky77.pizzaorderappreview.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.clearsky77.pizzaorderappreview.R
 import com.clearsky77.pizzaorderappreview.datas.StoreData
 
 class PizzaStoreAdapter(
@@ -9,4 +13,16 @@ class PizzaStoreAdapter(
     val resId : Int, // R.으로 찾는 것의 인트로된 아이디
     val mList : ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext) // inflate 해주는 도구
+
+    // 우리가 원하는 모양으로 보여주기 위해 getView를 오버라이딩
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView // convertView 그대로 쓰기 어려워서 일단 변수로 담아준다.
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.pizza_store_list_item, null) // 해당 xml을 인스턴스로 만들어준다.
+        }
+        val row = tempRow!! // !!는 널이 아니라는 뜻
+        return row
+    }
 }

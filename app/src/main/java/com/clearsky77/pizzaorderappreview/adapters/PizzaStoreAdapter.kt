0235e962a147998cbf070ab168973a184aa4641c
clearsky77 <clearsky3264@gmail.com>
2021-10-12 19:14:31 +0900

    피자가게 리스트 표현하는 어댑터 작업(주석 추가)

--- app/src/main/java/com/clearsky77/pizzaorderappreview/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/adapters/PizzaStoreAdapter.kt
@@ -5,9 +5,8 @@ import android.widget.ArrayAdapter
 import com.clearsky77.pizzaorderappreview.datas.StoreData
 
 class PizzaStoreAdapter(
-    val mContext : Context,
-    val resId : Int,
+    val mContext : Context, // 어느 화면이 쓰는지 알려달라
+    val resId : Int, // R.으로 찾는 것의 인트로된 아이디
     val mList : ArrayList<StoreData>
-
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
 }

39155c0e01e92a0bb7227ff4ed2fa6fe737119bd
clearsky77 <clearsky3264@gmail.com>
2021-10-13 17:12:01 +0900

    닉네임 변경 화면 추가

--- app/src/main/java/com/clearsky77/pizzaorderappreview/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/fragments/MyProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.clearsky77.pizzaorderappreview.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
 
@@ -20,6 +21,9 @@ class MyProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        editNicknameBtn.setOnClickListener {
+
+        }
 
     }
 }

392ac54bb3a5793a060695b9b5e7db8f6cc0bcf4
clearsky77 <clearsky3264@gmail.com>
2021-10-13 16:32:04 +0900

    전화걸기 누르면 -> 전화(CALL)권한 확인  - TedPermission으로 권한 체크  - 거절 상황만 코딩 완료

--- app/src/main/java/com/clearsky77/pizzaorderappreview/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/ViewStoreDetailActivity.kt
@@ -1,10 +1,16 @@
 package com.clearsky77.pizzaorderappreview
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.clearsky77.pizzaorderappreview.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import android.widget.Toast
+
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,23 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         Glide.with(this).load(storeData.logoURL).into(logoImg)
         storeNameTxt.text = storeData.name
         phoneNum.text = storeData.phoneNum
+
+
+        callBtn.setOnClickListener {
+            //val permissionlistener: PermissionListener = object : PermissionListener {
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+                }
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@ViewStoreDetailActivity, "통화 권한이 거절되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            TedPermission.create()
+                .setPermissionListener(pl) // pl에 적힌대로 해주세요
+                .setPermissions(Manifest.permission.CALL_PHONE) // 매니페스트에 콜 권한이 있어요.
+                .check() // 이걸 물어봐주세요
+
+        }
+
     }
 }

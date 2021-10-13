a1fb2fe4efbb584c7df615164d3be1726c3e310e
clearsky77 <clearsky3264@gmail.com>
2021-10-13 16:16:24 +0900

    가게 상세화면에서 가게 상세정보 데이터 표시

--- app/src/main/java/com/clearsky77/pizzaorderappreview/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/ViewStoreDetailActivity.kt
@@ -2,10 +2,19 @@ package com.clearsky77.pizzaorderappreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.clearsky77.pizzaorderappreview.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        val storeData = intent.getSerializableExtra("store") as StoreData // serializable된 것을 캐스팅
+
+        Glide.with(this).load(storeData.logoURL).into(logoImg)
+        storeNameTxt.text = storeData.name
+        phoneNum.text = storeData.phoneNum
     }
 }
--- app/src/main/java/com/clearsky77/pizzaorderappreview/datas/StoreData.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/datas/StoreData.kt
@@ -6,6 +6,7 @@ class StoreData(
     val name : String,
     val phoneNum : String,
     val logoURL : String
-) : Serializable //getExtra가 가능한 자료형처럼 보이게 하기 -> Serializable
+) : Serializable // intent에서 putExtra한 것을 getExtra가 가능하도도록
+                // (지원 가능한 자료형이 따로 있다) 지원되는 자료형처럼 보이게 하기 -> Serializable
 {
 }

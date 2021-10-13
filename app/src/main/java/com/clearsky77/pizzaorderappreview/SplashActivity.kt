b888e2913e198bc07c98ababcd78630124512454
clearsky77 <clearsky3264@gmail.com>
2021-10-13 19:38:03 +0900

    Hander / postDelayed 함수 활용, 3초 후에 메인 화면으로 이동

--- app/src/main/java/com/clearsky77/pizzaorderappreview/SplashActivity.kt
+++ app/src/main/java/com/clearsky77/pizzaorderappreview/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.clearsky77.pizzaorderappreview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000) // 3초 후에 다음 작업을 시작하라.
     }
 }
